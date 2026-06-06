8d3f3091e0cee2f4314ed51eef3c4a405fe6d306
Armando Ruiz <armandorl@gmail.com>
2026-06-06 08:45:47 +0200

    Added prerequisities for build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+#python3 -m venv ~/zephyrproject/.venv
+#Activate the virtual environment:
+#source ~/zephyrproject/.venv/bin/activate
+
 west build -p always -b s32g274ardb/s32g274a/m7 samples/basic/minimal -DCONFIG_DEBUG=y
