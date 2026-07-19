b8a01b33dc3bc8dbeff0586e83ddfc312f5684c4
Armando Ruiz <armandorl@gmail.com>
2026-07-19 20:50:04 +0200

    Git ignore project

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 #Activate the virtual environment:
 #source ~/zephyrproject/.venv/bin/activate
 
-west build -p always -b s32g274ardb/s32g274a/m7 samples/basic/minimal -DCONFIG_DEBUG=y
+west build -p always -b s32g274ardb/s32g274a/m7 project/zeus -DCONFIG_DEBUG=y
 python3 scripts/mk_ivt.py scripts/s32g274ardb_flash_image.cfg
